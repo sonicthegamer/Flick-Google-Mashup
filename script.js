45367a397175761c4500cc8f4754212938284eed
sonicthegamer <joelcolon1337@Gmail.com>
2016-07-06 18:44:24 +0000

    integration

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    
+
+
+
+// BASE MAPS STYLED MAPS SATELLITE DIRECTIONS MARKERS
+
+function initMap() {
+  // Create a map object and specify the DOM element for display.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    scrollwheel: false,
+    zoom: 8
+  });
+}
+
+
+
+;
+
+
+
+$('#clear').click(function{
+  
+});
+  
+
+
+
+// AIzaSyBOfQ-p4jlF0taLr02WGLorfjbgyveKjtg
+
+});
