02d63d6188b53b250e48a1dd23ba5852e4c6d8b8
sonicthegamer <joelcolon1337@Gmail.com>
2016-07-07 18:32:06 +0000

    luis sucks

--- script.js
+++ script.js
@@ -1,13 +1,60 @@
+$(document).ready(function (){
 
-var script = document.createElement('script');
-script.src = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp';
-document.getElementsByTagName('head')[0].appendChild(script);
+$("#Submit").click(function(){
+    var userInput = $('#userInput').val();
+    $.getJSON("https://api.flickr.com/services/rest/?method=flickr.photos.search&has_geo=1&extras=geo&api_key=eea149dc8bcfd608a8d301aecd9dde86&tags=" + userInput + "&tagmode=any&format=json&jsoncallback=?",
+    function(data){
+        console.log(data);
+        var photos = []
+        photos[0] = {
+          latitude:data.photos.photo[0].latitude,
+          longitude:data.photos.photo[0].longitude,
+        }
+        console.log(photos[0]);
+    });
+})
+
+});
+         
+     function initMap() { 
+        var myLatLng = {photos[0].latitude  , lng:photos.longitude};
+        // var 3ndLatLng = {lat:20.700 , lng: -29.923};
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 7,
+          center: myLatLng
+        });
+        var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+        
+        var marker = new google.maps.Marker({
+          position: myLatLng,
+          map: map,
+          icon: image
+        });
+        var marker = new google.maps.Marker({
+        //   position: 3ndLatLng,
+          map: map,
+          title: 'Hello World!'
+        });
+      }
+
+
+
+
+
+
+
+
+
     
 
 
 
 
+//key eea149dc8bcfd608a8d301aecd9dde86
 
+//Secret:
+//042027c86592d645
 
 
 
