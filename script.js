88dffe8d49ce2b7d4890232c400838de9232b1d0
sonicthegamer <joelcolon1337@Gmail.com>
2016-07-08 13:39:12 +0000

    luis sucks x2

--- script.js
+++ script.js
@@ -1,46 +1,74 @@
+ 
+var map;
+
+
+
+     function initMap() { 
+        //var myLatLng = {lat:photos[0].latitude, lng:photos[0].longitude};
+
+          map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 7,
+          styles: [
+             {
+    featureType: "all",
+    stylers: [
+      { saturation: 20 }
+    ]
+  },{
+    featureType: "road.arterial",
+    elementType: "geometry",
+    stylers: [
+      { hue: "#ff66cc" },
+      { saturation: 500 }
+    ]
+  },{
+    featureType: "poi.business",
+    elementType: "labels",
+    stylers: [
+      { visibility: "off" }
+    ]
+  }
+            ]
+        });
+        
+
+
+      }
+
+
 $(document).ready(function (){
 
 $("#Submit").click(function(){
     var userInput = $('#userInput').val();
-    $.getJSON("https://api.flickr.com/services/rest/?method=flickr.photos.search&has_geo=1&extras=geo&api_key=eea149dc8bcfd608a8d301aecd9dde86&tags=" + userInput + "&tagmode=any&format=json&jsoncallback=?",
+    $.getJSON("https://api.flickr.com/services/rest/?method=flickr.photos.search&has_geo=1&extras=geo, url_o&api_key=eea149dc8bcfd608a8d301aecd9dde86&tags=" + userInput + "&tagmode=any&format=json&jsoncallback=?",
     function(data){
-        console.log(data);
-        var photos = []
-        photos[0] = {
-          latitude:data.photos.photo[0].latitude,
-          longitude:data.photos.photo[0].longitude,
-        }
-        console.log(photos[0]);
-    });
-})
+   //     console.log(data);
+          
+          
+          
+  //      var photos = [];
+    //    photos[0] = {
+      //    latitude:data.photos.photo[0].latitude,
+        //  longitude:data.photos.photo[0].longitude,
+          //url:data.photos.photo[0].url_o,
+      //  }
 
-});
-         
-     function initMap() { 
-        var myLatLng = {photos[0].latitude  , lng:photos.longitude};
-        // var 3ndLatLng = {lat:20.700 , lng: -29.923};
-
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 7,
-          center: myLatLng
-        });
+        var lat = photos[0].latitude;
+        var long = photos[0].longitude;
+        console.log(lat,long);
+         var myLatLng = {lat:5,lng:5};
         var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
-        
-        var marker = new google.maps.Marker({
+
+          var marker = new google.maps.Marker({
           position: myLatLng,
           map: map,
           icon: image
         });
-        var marker = new google.maps.Marker({
-        //   position: 3ndLatLng,
-          map: map,
-          title: 'Hello World!'
-        });
-      }
+    });
+});
 
 
-
-
+    
 
 
 
@@ -64,3 +92,4 @@ $("#Submit").click(function(){
 
 
 // AIzaSyBOfQ-p4jlF0taLr02WGLorfjbgyveKjtg
+});
